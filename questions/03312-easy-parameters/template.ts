bfe4ea4bfe89ce3f438c7bcacd1ad96b7bd4a614
techino <git@techinol.com>
2024-11-29 07:20:46 +0900

    solved-with-research: 2 Get Return Type

--- questions/03312-easy-parameters/template.ts
+++ questions/03312-easy-parameters/template.ts
@@ -1,1 +1,4 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => unknown> =
+  T extends (...args: infer Args) => unknown
+    ? Args
+    : never
