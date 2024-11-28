f329e36a190f9353e8be497b68d894790b925be7
techino <git@techinol.com>
2024-11-29 06:55:51 +0900

    solved: 898	Includes

--- questions/00898-easy-includes/template.ts
+++ questions/00898-easy-includes/template.ts
@@ -1,1 +1,8 @@
-type Includes<T extends readonly any[], U> = any
+import type { Equal } from '@type-challenges/utils'
+
+export type Includes<T extends readonly unknown[], U> =
+  T extends [infer Head, ...infer Rest]
+    ? Equal<Head, U> extends true
+      ? true
+      : Includes<Rest, U>
+    : false
