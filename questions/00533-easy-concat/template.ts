bee5deff45bfc696fa8a1820c1d94de6ad8653ef
techino <git@techinol.com>
2024-11-29 06:31:25 +0900

    solved: 533	Concat

--- questions/00533-easy-concat/template.ts
+++ questions/00533-easy-concat/template.ts
@@ -1,1 +1,4 @@
-type Concat<T, U> = any
+type Concat<T extends readonly unknown[], U extends readonly unknown[]> =
+  U extends readonly [infer Head, ...infer Rest]
+    ? Concat<[...T, Head], Rest>
+    : T
